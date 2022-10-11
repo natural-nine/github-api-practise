2246b456109dc3aa646b62914f617283e1a14aee
natural-nine <wan.jun.bruce@gmail.com>
2022-10-11 21:37:21 +0900

    test..

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import App from "./App";
@@ -7,12 +8,15 @@ import App from "./App";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+const queryClient = new QueryClient();
 root.render(
-  <RecoilRoot>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </RecoilRoot>
+  <QueryClientProvider client={queryClient}>
+    <RecoilRoot>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </RecoilRoot>
+  </QueryClientProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import { useInfiniteQuery } from "react-query";
+import styled from "styled-components";
+import instance from "../shared/axios";
 
 const Home = () => {
-    return(
-        <h1>Home Page</h1>
-    )
+  interface repoData {
+    id: number;
+    created_at: string;
+  }
+  const [isSearch, setIsSearch] = useState("react");
+  const getQuery = async () => {
+    const { data } = await instance.get(
+      `/search/repositories?q=${isSearch}&per_page=100&page=${1}`
+    );
+    return {
+      data,
+      //   nextPage: pageParams + 1,
+    };
+  };
+  const { data, isLoading } = useInfiniteQuery(
+    ["repo", isSearch],
+    async ({ pageParam = 1 }) => {
+      return await getQuery();
+    },
+    {
+      refetchOnWindowFocus: false,
+      refetchOnMount: true,
+      enabled: !!isSearch,
+      // getNextPageParam: (lastPage) => {
+      //   return lastPage.nextPage;
+      // },
+    }
+  );
+  console.log(isLoading);
+  console.log(data);
+  return <Wrap>Home Page</Wrap>;
 };
 
+
+const Wrap = styled.div`
+    width: 75%;
+    margin: auto;
+    background-color: red;
+
+`
 export default Home;
