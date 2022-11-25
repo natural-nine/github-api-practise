0dfaece83ed8b1325bade662bc7151218b3708ef
natural-nine <wan.jun.bruce@gmail.com>
2022-11-25 22:56:57 +0900

    Style: 코드 포맷팅

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,47 +1,14 @@
 import React, { useState } from "react";
-import { useInfiniteQuery } from "react-query";
 import styled from "styled-components";
-import instance from "../shared/axios";
 
 const Home = () => {
-  interface repoData {
-    id: number;
-    created_at: string;
-  }
-  const [isSearch, setIsSearch] = useState("react");
-  const getQuery = async () => {
-    const { data } = await instance.get(
-      `/search/repositories?q=${isSearch}&per_page=100&page=${1}`
-    );
-    return {
-      data,
-      //   nextPage: pageParams + 1,
-    };
-  };
-  const { data, isLoading } = useInfiniteQuery(
-    ["repo", isSearch],
-    async ({ pageParam = 1 }) => {
-      return await getQuery();
-    },
-    {
-      refetchOnWindowFocus: false,
-      refetchOnMount: true,
-      enabled: !!isSearch,
-      // getNextPageParam: (lastPage) => {
-      //   return lastPage.nextPage;
-      // },
-    }
-  );
-  console.log(isLoading);
-  console.log(data);
-  return <Wrap>Home Page</Wrap>;
+  return <Wrap></Wrap>;
 };
 
-
 const Wrap = styled.div`
-    width: 75%;
-    margin: auto;
-    background-color: red;
-
-`
+  width: 85%;
+  height: auto;
+  margin: auto;
+  background-color: gray;
+`;
 export default Home;
--- src/pages/Save.tsx
+++ src/pages/Save.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Save = () => {
-  return (<h1>Save Page</h1>);
+  return <h1>Save Page</h1>;
 };
 
 export default Save;
