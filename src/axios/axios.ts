d3897a3061cb5feceaac9b38f78da88c405c0f37
natural-nine <wan.jun.bruce@gmail.com>
2023-01-26 20:02:20 +0900

    delete token

--- src/axios/axios.ts
+++ src/axios/axios.ts
@@ -5,14 +5,14 @@ const instance = axios.create({
   headers: {},
 });
 
-instance.interceptors.request.use(
-  (config: any) => {
-    config.headers.Authorization = `Bearer ${process.env.REACT_APP_API_KEY}`;
-    return config;
-  },
-  error => {
-    return Promise.reject(error);
-  }
-);
+// instance.interceptors.request.use(
+//   (config: any) => {
+//     config.headers.Authorization = `Bearer ${process.env.REACT_APP_API_KEY}`;
+//     return config;
+//   },
+//   error => {
+//     return Promise.reject(error);
+//   }
+// );
 
 export default instance;
